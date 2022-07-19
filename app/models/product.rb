fb4a88de3339bf244a8df2e68bfb5576ba5b7b67
FredrickBarrett <f.barrett12@gmail.com>
2022-07-19 11:03:49 -0400

    added relationship between product and properties

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
     validates :name, uniqueness: true
     validates :upc, uniqueness: true
     validates :upc, numericality: { only_integer: true}
+
+    has_many :properties, dependent: :destroy
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,4 @@
 class Property < ApplicationRecord
+    validates :name, uniqueness: true
+    belongs_to :product, dependent: :destroy
 end
